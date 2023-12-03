73d6d7088f015c7be65739c1bbce14bbb4b9d69a
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2023-12-03 15:27:27 +0100

    feat: Day 3

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,11 +1,141 @@
+use std::{collections::HashSet, usize};
+
 pub fn part1() -> String {
     let path = "input/day3.txt";
     let input = std::fs::read_to_string(path).unwrap();
-    "".to_string()
+
+    let mut board = vec![vec![]];
+
+    for line in input.lines() {
+        let mut line_vec = vec![];
+        let mut i = 0;
+        while i < line.len() {
+            let c = line.chars().nth(i).unwrap();
+            match c {
+                '.' => {
+                    line_vec.push(0);
+                    i += 1;
+                }
+                x if x.is_ascii_digit() => {
+                    let mut number = String::new();
+                    number.push(c);
+                    let mut k = 1;
+                    while i + k < line.len() && line.chars().nth(i + k).unwrap().is_ascii_digit() {
+                        number.push(line.chars().nth(i + k).unwrap());
+                        k += 1;
+                    }
+                    for _ in 0..k {
+                        line_vec.push(number.parse::<i32>().unwrap());
+                    }
+                    i += k;
+                }
+                _ => {
+                    line_vec.push(-1);
+                    i += 1;
+                }
+            }
+        }
+
+        board.push(line_vec);
+    }
+    count_numbers(&mut board).to_string()
+}
+
+fn adjacents(i: usize, j: usize, board: &Vec<Vec<i32>>) -> Vec<(usize, usize)> {
+    let mut adjacents = Vec::new();
+    for x in i.saturating_sub(1)..=i + 1 {
+        for y in j.saturating_sub(1)..=j + 1 {
+            if (x != i || y != j) && x < board.len() && y < board[x].len() {
+                adjacents.push((x, y));
+            }
+        }
+    }
+
+    adjacents
+}
+
+fn count_numbers(input: &mut Vec<Vec<i32>>) -> i32 {
+    let mut count = 0;
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            if input[i][j] > 0 {
+                for (x, y) in adjacents(i, j, input) {
+                    if input[x][y] == -1 {
+                        count += input[i][j];
+                        let mut k = 0;
+                        let initilal = input[i][j];
+                        while j + k < input[i].len() && input[i][j + k] == initilal {
+                            input[i][j + k] = 0;
+                            k += 1;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    count
 }
 
 pub fn part2() -> String {
     let path = "input/day3.txt";
     let input = std::fs::read_to_string(path).unwrap();
-    "".to_string()
+
+    let mut board = vec![vec![]];
+
+    for line in input.lines() {
+        let mut line_vec = vec![];
+        let mut i = 0;
+        while i < line.len() {
+            let c = line.chars().nth(i).unwrap();
+            match c {
+                '*' => {
+                    line_vec.push(-1);
+                    i += 1;
+                }
+                x if x.is_ascii_digit() => {
+                    let mut number = String::new();
+                    number.push(c);
+                    let mut k = 1;
+                    while i + k < line.len() && line.chars().nth(i + k).unwrap().is_ascii_digit() {
+                        number.push(line.chars().nth(i + k).unwrap());
+                        k += 1;
+                    }
+                    for _ in 0..k {
+                        line_vec.push(number.parse::<i32>().unwrap());
+                    }
+                    i += k;
+                }
+                _ => {
+                    line_vec.push(0);
+                    i += 1;
+                }
+            }
+        }
+
+        board.push(line_vec);
+    }
+
+    count_numbers2(&mut board).to_string()
+}
+
+fn count_numbers2(input: &mut Vec<Vec<i32>>) -> i32 {
+    let mut count = 0;
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            if input[i][j] == -1 {
+                let mut found = HashSet::new();
+                for (x, y) in adjacents(i, j, input) {
+                    if input[x][y] > 0 {
+                        found.insert(input[x][y]);
+                    }
+                }
+                if found.len() == 2 {
+                    let found = found.iter().collect::<Vec<&i32>>();
+                    count += found[0] * found[1];
+                }
+            }
+        }
+    }
+    count
 }
